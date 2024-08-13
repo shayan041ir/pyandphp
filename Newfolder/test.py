0862de7fb297ea1a18c47e3071aa0707acc914a7
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-08-14 00:07:37 +0330

    Delete Newfolder directory

--- Newfolder/test.py
+++
@@ -1,5 +0,0 @@
-from hezar.models import Model
-
-model = Model.load("hezarai/crnn-fa-license-plate-recognition")
-plate_text = model.predict("./upload/img/nemayedakheli/shayantest.jpg")
-print(plate_text)  # Persian text of mixed numbers and characters might not show correctly in the console
