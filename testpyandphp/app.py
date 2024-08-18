b14717b45e7494f8b304c9051f7eab7403a12cad
morteza noei <mortezanoei80@gmail.com>
2024-08-18 13:07:04 +0330

    Update app.py

--- testpyandphp/app.py
+++ testpyandphp/app.py
@@ -13,11 +13,18 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 def process_image(filepath):
-    model = Model.load("hezarai/crnn-fa-license-plate-recognition")
-    plate_text = model.predict(filepath)
-    sli=slice(11,-3)
-    numberOfPlate=str(plate_text)[sli]
-    return numberOfPlate#returns string
+  image=Image.open('path/to/file')
+  higit,withd=image.size
+  crop=(int(withd*0.15),int(higit*0.15),withd+int(withd*0.40),higit-int(higit*0.50))
+  Cimage=image.crop(crop)
+  Cimage.show()
+  model = Model.load("hezarai/crnn-fa-license-plate-recognition")
+  plate_text = model.predict(Cimage)
+  sli=slice(11,-3)
+  numberOfPlate=str(plate_text)[sli]
+  sli2,sli3=slice(0,-2),slice(-2,len(numberOfPlate))
+  numberOfPlate=str(numberOfPlate[sli2]+"-"+numberOfPlate[sli3])
+  return numberOfPlate#returns string
 
 
 @app.route('/upload', methods=['POST'])
